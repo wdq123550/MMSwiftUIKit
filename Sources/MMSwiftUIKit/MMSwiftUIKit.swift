82ed27b8196cda9f3fe53109878c226f938c550a
Kim <wdq123550@qq.com>
2025-05-28 12:41:12 +0800

    修正部分细节

--- Sources/MMSwiftUIKit/MMSwiftUIKit.swift
+++ Sources/MMSwiftUIKit/MMSwiftUIKit.swift
@@ -38,31 +38,34 @@ public enum MMGradientDirection {
     case topRightToBottomLeft     // 右上到左下
 }
 
-
-public class ButtonItem {
-    var font: UIFont?
-    var title: String?
-    var titleColor: UIColor?
-    var attributeText: NSAttributedString?
-    var image: UIImage?
-    var imagePosition: NSDirectionalRectEdge?
-    var spacing: CGFloat?
-    var bgColor: UIColor?
-    var bgImage: UIImage?
-    var state: UIControl.State
-    var contentInsets: NSDirectionalEdgeInsets?
+public extension MMButton {
     
-    public init(font: UIFont? = nil, title: String? = nil, titleColor: UIColor? = nil, attributeText: NSAttributedString? = nil, image: UIImage? = nil, imagePosition: NSDirectionalRectEdge? = nil, spacing: CGFloat? = nil, bgColor: UIColor? = nil, bgImage: UIImage? = nil, state: UIControl.State, contentInsets: NSDirectionalEdgeInsets? = nil) {
-        self.font = font
-        self.title = title
-        self.titleColor = titleColor
-        self.attributeText = attributeText
-        self.image = image
-        self.imagePosition = imagePosition
-        self.spacing = spacing
-        self.bgColor = bgColor
-        self.bgImage = bgImage
-        self.state = state
-        self.contentInsets = contentInsets
+    class ButtonItem {
+        var font: UIFont?
+        var title: String?
+        var titleColor: UIColor?
+        var attributeText: NSAttributedString?
+        var image: UIImage?
+        var imagePosition: NSDirectionalRectEdge?
+        var spacing: CGFloat?
+        var bgColor: UIColor?
+        var bgImage: UIImage?
+        var state: UIControl.State
+        var contentInsets: NSDirectionalEdgeInsets?
+        
+        public init(font: UIFont? = nil, title: String? = nil, titleColor: UIColor? = nil, attributeText: NSAttributedString? = nil, image: UIImage? = nil, imagePosition: NSDirectionalRectEdge? = nil, spacing: CGFloat? = nil, bgColor: UIColor? = nil, bgImage: UIImage? = nil, state: UIControl.State, contentInsets: NSDirectionalEdgeInsets? = nil) {
+            self.font = font
+            self.title = title
+            self.titleColor = titleColor
+            self.attributeText = attributeText
+            self.image = image
+            self.imagePosition = imagePosition
+            self.spacing = spacing
+            self.bgColor = bgColor
+            self.bgImage = bgImage
+            self.state = state
+            self.contentInsets = contentInsets
+        }
     }
+
 }
