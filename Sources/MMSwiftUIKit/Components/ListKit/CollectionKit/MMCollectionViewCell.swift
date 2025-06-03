e769a1df60a6cee00aaf7dbe529106f2da990edb
Kim <wdq123550@qq.com>
2025-06-03 13:20:06 +0800

    添加cell更新机制

--- Sources/MMSwiftUIKit/Components/ListKit/CollectionKit/MMCollectionViewCell.swift
+++ Sources/MMSwiftUIKit/Components/ListKit/CollectionKit/MMCollectionViewCell.swift
@@ -13,8 +13,13 @@ open class MMCVCellVM: NSObject, MMCVCellVMProtocol {
     open var cellType: MMCVCellProtocol.Type{ MMCVCell.self }
     public var indexPath: IndexPath?
     public var cellSize: CGSize?
+    
     public let clickSubject = PassthroughSubject<Void, Never>()
     public var cancellable = Set<AnyCancellable>()
+    
+    fileprivate let updateCellSubject = PassthroughSubject<Void, Never>()
+    fileprivate var isConfigUpdateAction = false
+    public func updateCell() { self.updateCellSubject.send() }
 }
 
 open class MMCVCell: UICollectionViewCell, MMCVCellProtocol {
@@ -25,12 +30,18 @@ open class MMCVCell: UICollectionViewCell, MMCVCellProtocol {
     public override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .clear
+        self.bind()
+    }
+    
+    private func bind() {
+        //点击事件
         let tapGes = UITapGestureRecognizer()
+        self.contentView.addGestureRecognizer(tapGes)
         tapGes.publisher.sink { [unowned self] tapGes in
             self.onClickContentView()
         }.store(in: &cancellabel)
-        self.contentView.addGestureRecognizer(tapGes)
     }
+    
     required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -40,6 +51,15 @@ open class MMCVCell: UICollectionViewCell, MMCVCellProtocol {
     open func reset(with cellVM: MMCVCellVMProtocol, indexPath: IndexPath) {
         self._viewModel = cellVM as? MMCVCellVM
         self._viewModel?.indexPath = indexPath
+        
+        if self._viewModel?.isConfigUpdateAction == false {
+            if let viewModel = self._viewModel, let indexPath = viewModel.indexPath {
+                viewModel.updateCellSubject.sink { [unowned viewModel, self] _ in
+                    self.reset(with: viewModel, indexPath: indexPath)
+                }.store(in: &cancellabel)
+                viewModel.isConfigUpdateAction = true
+            }
+        }
     }
     
     @discardableResult open class func cellSize(with cellVM: MMCVCellVMProtocol, indexPath: IndexPath) -> CGSize {
--- Sources/MMSwiftUIKit/Components/ListKit/TableKit/MMTableViewCell.swift
+++ Sources/MMSwiftUIKit/Components/ListKit/TableKit/MMTableViewCell.swift
@@ -10,26 +10,39 @@ import UIKit
 import Combine
 
 open class MMTVCellVM: NSObject, MMTVCellVMProtocol{
+    //protocol
     public var cellHeight: CGFloat?
     public var indexPath: IndexPath?
     open var cellType: MMTVCellProtocol.Type{ MMTVCell.self }
+    
+    //click
     public let clickSubject = PassthroughSubject<Void, Never>()
+    
+    // cancellabel
     public var cancellable = Set<AnyCancellable>()
+    
+    //update cell
+    fileprivate let updateCellSubject = PassthroughSubject<Void, Never>()
+    fileprivate var isConfigUpdateAction = false
+    public func updateCell() { self.updateCellSubject.send() }
+    
 }
 
 open class MMTVCell: UITableViewCell, MMTVCellProtocol {
     
     public var cancellabel = Set<AnyCancellable>()
     public var mm: MMTableViewCellChain<MMTVCell> { MMTableViewCellChain(view: self) }
-    
     public var _viewModel: MMTVCellVM?
     
     public required init(reuserID: String) {
         super.init(style: .default, reuseIdentifier: reuserID)
-        self.mm
-            .selectionStyle(.none)
-            .backgroundColor(.clear)
-        
+        self.selectionStyle = .none
+        self.backgroundColor = .clear
+        self.bind()
+    }
+    
+    private func bind() {
+        //点击事件
         let tapGes = UITapGestureRecognizer()
         self.contentView.addGestureRecognizer(tapGes)
         tapGes.publisher.sink { [unowned self] tapGes in
@@ -44,6 +57,14 @@ open class MMTVCell: UITableViewCell, MMTVCellProtocol {
     open func reset(with cellVM: MMTVCellVMProtocol, indexPath: IndexPath) {
         self._viewModel = cellVM as? MMTVCellVM
         self._viewModel?.indexPath = indexPath
+        if self._viewModel?.isConfigUpdateAction == false {
+            if let viewModel = self._viewModel, let indexPath = viewModel.indexPath {
+                viewModel.updateCellSubject.sink { [unowned viewModel, self] _ in
+                    self.reset(with: viewModel, indexPath: indexPath)
+                }.store(in: &cancellabel)
+                viewModel.isConfigUpdateAction = true
+            }
+        }
     }
     
     @discardableResult open class func cellHeight(with cellVM: MMTVCellVMProtocol, indexPath: IndexPath) -> CGFloat {
